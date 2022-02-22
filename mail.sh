5ee51caea01339435e70ce82903a47c99f9496c0
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-02-22 16:48:47 +0100

    Create mail.sh

---
+++ mail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#postfix install
+sudo wget https://raw.githubusercontent.com/Tikijavi/Mail-terraform/main/postfix-confjs.sh
+sudo sh postfix-confjs.sh
+sudo apt-get install -q postfix -y
+sudo postconf -e "home_mailbox= Maildir/"
+#dovecot install
+sudo apt-get install dovecot-core dovecot-imapd dovecot-pop3d -y
+sudo wget https://raw.githubusercontent.com/Tikijavi/Mail-terraform/main/10-mail.conf
+sudo wget https://raw.githubusercontent.com/Tikijavi/Mail-terraform/main/10-auth.conf
+sudo systemctl restart postfix
+sudo systemctl restart dovecot
