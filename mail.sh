bcf34e2eee57680e1dac7c0430a9738fbfceb7c8
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-02-22 17:32:33 +0100

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -4,6 +4,9 @@ sudo hostnamectl set-hostname mailserver
 sudo rm /etc/hosts
 echo "127.0.0.1 localhost" | sudo tee /etc/hosts
 echo "127.0.0.1 mailserver.jdajs.cf mailserver" | sudo tee -a /etc/hosts
+#users
+sudo useradd -m test1
+echo test1:qweR1234 | sudo chpasswd
 #postfix install
 sudo wget https://raw.githubusercontent.com/Tikijavi/Mail-terraform/main/postfix-confjs.sh
 sudo sh postfix-confjs.sh
